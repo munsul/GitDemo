e34a10c7987b9e24a77107498af530e0a2a7a39c
munsul <monica.sasuluya98@gmail.com>
2021-07-20 10:21:56 +0800

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void posJira()
+	{
+		System.out.println("posJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,25 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//testng xml file from Maven
+	//how to eexecute all test cases from Test folder - mvn test
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+	
+	
+
+}
